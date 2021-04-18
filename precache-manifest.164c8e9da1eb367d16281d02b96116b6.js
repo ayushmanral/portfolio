a4b6bf9f262ab9efbabab0739b234e38a9c23a40
Ayush Manral <manralayush1@gmail.com>
2021-04-18 20:31:23 +0530

    Updates

---
+++ precache-manifest.164c8e9da1eb367d16281d02b96116b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5f2d222828817e4d9826930055a1180",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8484de810a5e2a7b1820",
+    "url": "/static/css/main.27817e2a.chunk.css"
+  },
+  {
+    "revision": "97e63636bc8fb388b7b4",
+    "url": "/static/js/2.ca42dc3b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ca42dc3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8484de810a5e2a7b1820",
+    "url": "/static/js/main.021cc2f8.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  }
+]);
